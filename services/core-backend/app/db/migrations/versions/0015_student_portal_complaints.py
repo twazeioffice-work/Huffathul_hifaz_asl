ab8cfde8ff4db8de1d7eb3a3997fabddecd3bfa8
twazeibyfarmfresh-cloud <twazeibyfarmfresh@gmail.com>
2026-08-23 11:13:11 +0530

    fix(db): correct alembic revision ID to 0015

--- services/core-backend/app/db/migrations/versions/0015_student_portal_complaints.py
+++ services/core-backend/app/db/migrations/versions/0015_student_portal_complaints.py
@@ -11,8 +11,8 @@ import sqlalchemy as sa
 import os
 
 # revision identifiers, used by Alembic.
-revision: str = '0015_student_portal_complaints'
-down_revision: Union[str, None] = '0014_branding_engine'
+revision: str = '0015'
+down_revision: Union[str, None] = '0014'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
